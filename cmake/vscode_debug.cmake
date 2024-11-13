78878c6183850320e313961deae80ef10bc16c48
stephendpmurphy <stephendpmurphy@msn.com>
2024-11-13 09:36:20 -0500

    Add CMake modules to generate VSCode Launch and Settings files

---
+++ cmake/vscode_debug.cmake
@@ -0,0 +1,47 @@
+# Make sure to create the .vscode directory if it doesn't exist
+file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/.vscode)
+
+string(TOLOWER ${DEBUG_TOOLSET} DEBUG_SERVER_TYPE)
+
+# Specify the template for the c_cpp_properties.json file
+write_file(${CMAKE_CURRENT_SOURCE_DIR}/.vscode/launch.json "
+{
+    \"version\":\"0.2.0\",
+    \"configurations\":[
+    {
+       \"name\":\"${DEBUG_TOOLSET}\",
+       \"type\":\"cortex-debug\",
+       \"request\":\"launch\",
+       \"servertype\":\"${DEBUG_SERVER_TYPE}\",
+       \"cwd\":\"\",
+       \"executable\":\"${EXECUTABLE_OUTPUT_PATH}/${EXECUTABLE_NAME}.elf\",
+       \"device\":\"${MCU_TARGET}\",
+        \"interface\": \"${DEBUG_INTERFACE}\",
+       \"v1\":false,
+       \"serverArgs\":[
+           \"--nogui\"
+       ],"
+)
+
+if(${ENABLE_SEGGER_RTT} AND (${DEBUG_SERVER_TYPE} STREQUAL "jlink"))
+
+write_file(${CMAKE_CURRENT_SOURCE_DIR}/.vscode/launch.json
+"       \"rttConfig\":{
+            \"enabled\":true,
+            \"address\":\"auto\",
+            \"decoders\":[{
+            \"port\":0,
+            \"type\":\"console\"
+            }]
+        }
+    }]
+}" APPEND)
+
+else()
+
+write_file(${CMAKE_CURRENT_SOURCE_DIR}/.vscode/launch.json
+"   }]
+}"
+APPEND)
+
+endif()
---
+++ cmake/vscode_intellisense.cmake
@@ -0,0 +1,32 @@
+# Make sure to create the .vscode directory if it doesn't exist
+file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/.vscode)
+
+# Note, if a seperate include variable is used for SDK includes you would append it here
+set(INTELLISENSE_INCLDUES ${INCLUDE_DIRS})
+
+# Generate the list of include directories as a JSON array
+set(INCLUDE_DIRS_JSON "")
+foreach(INCLUDE_DIR ${INTELLISENSE_INCLDUES})
+    list(APPEND INCLUDE_DIRS_JSON "\"${INCLUDE_DIR}\"")
+endforeach()
+string(JOIN ",\n" INCLUDE_DIRS_JSON ${INCLUDE_DIRS_JSON})
+
+# Specify the template for the c_cpp_properties.json file
+file(GENERATE OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/.vscode/c_cpp_properties.json CONTENT "
+{
+    \"configurations\": [
+        {
+            \"name\": \"${CMAKE_BUILD_TYPE}\",
+            \"includePath\": [
+                ${INCLUDE_DIRS_JSON}
+            ],
+            \"defines\": [\"\"],
+            \"compilerPath\": \"${CMAKE_C_COMPILER}\",
+            \"cStandard\": \"c99\",
+            \"cppStandard\": \"c++17\",
+            \"intelliSenseMode\": \"gcc-arm\"
+        }
+    ],
+    \"version\": 1
+}
+")
