fb08abd901ccb61daba5275fe9b6f79763bc9b22
stephendpmurphy <stephendpmurphy@msn.com>
2024-11-19 14:32:01 -0500

    Rework intellisense module to find all include dirs for the executable target automatically

--- cmake/vscode_intellisense.cmake
+++ cmake/vscode_intellisense.cmake
@@ -1,12 +1,33 @@
 # Make sure to create the .vscode directory if it doesn't exist
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/.vscode)
 
-# Note, if a seperate include variable is used for SDK includes you would append it here
-set(INTELLISENSE_INCLDUES ${INCLUDE_DIRS})
+get_target_property(LINKED_LIBRARIES ${EXECUTABLE_NAME} LINK_LIBRARIES)
+
+if(LINKED_LIBRARIES)
+    foreach(lib ${LINKED_LIBRARIES})
+        get_target_property(LIBRARIES_INCLUDE_DIRS ${lib} INCLUDE_DIRECTORIES)
+        if(LIBRARIES_INCLUDE_DIRS)
+            set(INTELLISENSE_INCLUDES ${INTELLISENSE_INCLUDES} ${LIBRARIES_INCLUDE_DIRS})
+        endif()
+    endforeach()
+
+    foreach(lib ${LINKED_LIBRARIES})
+        get_target_property(INTF_LIBRARIES_INCLUDE_DIRS ${lib} INTERFACE_INCLUDE_DIRECTORIES)
+        if(INTF_LIBRARIES_INCLUDE_DIRS)
+            set(INTELLISENSE_INCLUDES ${INTELLISENSE_INCLUDES} ${INTF_LIBRARIES_INCLUDE_DIRS})
+        endif()
+    endforeach()
+endif()
+
+get_target_property(APP_INCLUDE_DIRS ${EXECUTABLE_NAME} INCLUDE_DIRECTORIES)
+
+if(APP_INCLUDE_DIRS)
+    set(INTELLISENSE_INCLUDES ${INTELLISENSE_INCLUDES} ${APP_INCLUDE_DIRS})
+endif()
 
 # Generate the list of include directories as a JSON array
 set(INCLUDE_DIRS_JSON "")
-foreach(INCLUDE_DIR ${INTELLISENSE_INCLDUES})
+foreach(INCLUDE_DIR ${INTELLISENSE_INCLUDES})
     list(APPEND INCLUDE_DIRS_JSON "\"${INCLUDE_DIR}\"")
 endforeach()
 string(JOIN ",\n" INCLUDE_DIRS_JSON ${INCLUDE_DIRS_JSON})
